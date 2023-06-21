8f500ab1f06ec6200dccab330e1a5bd6e46b6caf
Amirreza Tabeshfard <Amirreza Tabeshfard@AMIRREZA-TABESH>
2023-06-22 01:14:31 +0330

    Create New Project: ATNetCoreTelegramBot.Web.UI (#21)

---
+++ ATNetCoreTelegramBot.ViewModels/ErrorViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ATNetCoreTelegramBot.ViewModels;
+
+public class ErrorViewModel : object
+{
+    public ErrorViewModel()
+    {
+
+    }
+
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
