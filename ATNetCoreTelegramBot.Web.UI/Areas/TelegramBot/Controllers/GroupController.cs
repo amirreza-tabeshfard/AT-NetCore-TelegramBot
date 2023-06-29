51aa4eef9f2159c26ae4f0c521565de5df1f29f3
Amirreza Tabeshfard <Amirreza Tabeshfard@AMIRREZA-TABESH>
2023-06-30 00:04:36 +0330

    1.0.0.8: Create Group [Create] (#33)

--- ATNetCoreTelegramBot.Web.UI/Areas/TelegramBot/Controllers/GroupController.cs
+++ ATNetCoreTelegramBot.Web.UI/Areas/TelegramBot/Controllers/GroupController.cs
@@ -14,6 +14,7 @@ namespace ATNetCoreTelegramBot.Web.UI.Areas.TelegramBot.Controllers
         private readonly IServiceScope _serviceScope;
         private readonly DAL.UnitOfWork _unitOfWork;
 
+        private Models.SchemaTelegram.Group? _group = default;
         private GroupViewModel? _groupVM = default;
 
         #endregion
@@ -33,6 +34,7 @@ namespace ATNetCoreTelegramBot.Web.UI.Areas.TelegramBot.Controllers
             _serviceScope = _serviceProvider.CreateScope();
 
             _unitOfWork = _serviceScope.ServiceProvider.GetRequiredService<DAL.UnitOfWork>();
+            _group = _serviceScope.ServiceProvider.GetRequiredService<Models.SchemaTelegram.Group>();
             _groupVM = _serviceScope.ServiceProvider.GetService<GroupViewModel>();
         }
 
@@ -48,7 +50,7 @@ namespace ATNetCoreTelegramBot.Web.UI.Areas.TelegramBot.Controllers
         #region Index
 
         [HttpGet]
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
             try
             {
@@ -78,6 +80,58 @@ namespace ATNetCoreTelegramBot.Web.UI.Areas.TelegramBot.Controllers
             }
 
             return View(_groupVM);
+        }
+
+        #endregion
+
+        #region Create
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(_group);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Ordering,InsertDateTime")] Models.SchemaTelegram.Group model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _group.Id = Guid.NewGuid();
+                    _group.Name = string.Concat("@",  model.Name);
+                    _group.Ordering = model.Ordering;
+                    _group.InsertDateTime = DateTime.Now;
+
+                    _unitOfWork
+                        .SchemaTelegramUnitOfWork
+                        .GroupRepository
+                        .Insert(_group)
+                        ;
+
+                    _unitOfWork
+                        .SaveChanges();
+
+                    return RedirectToAction(actionName: nameof(Index), controllerName: "Group", routeValues: new { Area = "TelegramBot" });
+                }
+            }
+            catch (Exception ex) when (ex.InnerException is HttpRequestException)
+            {
+                TelegramBotPageMessages.Add(new Infrastructure.TelegramBotPageMessage(alertSeverity: AlertSeverity.Error, title: "Service", $"هیچ ارتباطی با سرویس فوق برقرار نبوده و ارتباط ماشین با سرویس قطع می باشد.", "(عدم ارتباط با سرویس دهنده)"));
+                TelegramBotPageMessages.Add(new Infrastructure.TelegramBotPageMessage(alertSeverity: AlertSeverity.Information, title: "Service", $"ممکن است، سرویس فوق در حال به روز رسانی بوده و یا خاموش می باشد."));
+            }
+            catch (TimeoutException ex)
+            {
+                TelegramBotPageMessages.Add(new Infrastructure.TelegramBotPageMessage(alertSeverity: AlertSeverity.Error, title: "TimeoutException", ex.Message));
+            }
+            catch (Exception ex)
+            {
+                TelegramBotPageMessages.Add(new Infrastructure.TelegramBotPageMessage(alertSeverity: AlertSeverity.Error, title: "خطای سیستمی", "[ Message ]", ex.Message));
+            }
+
+            return View(_group);
         }
 
         #endregion
