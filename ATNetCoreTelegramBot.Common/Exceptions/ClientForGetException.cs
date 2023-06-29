6ab87c23e86cd7b3b14a66e81be5059e62bea64e
Amirreza Tabeshfard <Amirreza Tabeshfard@AMIRREZA-TABESH>
2023-06-30 01:04:37 +0330

    1.0.0.9: Create Group [Details] (#35)

---
+++ ATNetCoreTelegramBot.Common/Exceptions/ClientForGetException.cs
@@ -0,0 +1,14 @@
+﻿namespace ATNetCoreTelegramBot.Common.Exceptions;
+
+public class ClientForGetException : ApplicationException
+{
+    #region Constructor
+
+    public ClientForGetException(string message)
+        : base(message) { }
+
+    public ClientForGetException(string message, Exception exception)
+        : base(message, exception) { }
+
+    #endregion
+}
---
+++ ATNetCoreTelegramBot.ViewModels/ExceptionViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ATNetCoreTelegramBot.Common.Enums;
+
+namespace ATNetCoreTelegramBot.ViewModels;
+
+public class ExceptionViewModel : object
+{
+    public string? Title { get; set; }
+
+    public string? Message { get; set; }
+
+    public string? InnerException { get; set; }
+
+    public string? MessageHTML { get; set; }
+
+    public AlertSeverity AlertSeverity { get; set; }
+
+    public Alignment Alignment { get; set; }
+}
